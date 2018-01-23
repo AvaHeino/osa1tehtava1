c0320b5f833c76ac307efd57256f760473b6a4ce
AvaHeino <35535190+AvaHeino@users.noreply.github.com>
2018-01-23 12:38:58 +0200

    tehtava 1.3
    Tehtava 1.3

--- index.js
+++ index.js
@@ -1,52 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Otsikko = (props) => {
+const Hello = (props) => {
 	return (
-		<h1>{props.kurssiNimi}</h1>
+		<div>
+			<p>Hello {props.name}, you are {props.age} years old</p>
+		</div>
 		)
 }
 
-const Osa = (props) => {
-	return (
-		<p>{props.osa} {props.tehtavia}</p>
-	)
-}
-const Sisalto = (props) => {
-	return (
-		<div>
-			<Osa osa={props.osa1Nimi} tehtavia={props.osa1teht} />
-			<Osa osa={props.osa2Nimi} tehtavia={props.osa2teht} />
-			<Osa osa={props.osa3Nimi} tehtavia={props.osa3teht} />
-		</div>
-	)
-}
-
-const Yhteensa = (props) => {
-	return (
-		<p>yhteensa {props.tehtavaSumma} tehtavaa</p>
-	)
-}
 const App = () => {
-	const kurssi = 'Half Stack - sovelluskehitys'
-	const osa1 = 'Reactin perusteet'
-	const tehtavia1 = 10
-	const osa2 = 'Tiedonvalitys propseilla'
-	const tehtavia2 = 7
-	const osa3 = 'Komponenttien tila'
-	const tehtavia3 = 14
+	const nimi = 'Pekka'
+	const ika = 10
+   return (
+	<div>
+		<h1>Greetings</h1>
+		<Hello name="Arto"  age={26+10}/>
+		<Hello name={nimi} age={ika} />
+	</div>
 
-	return (
-		<div>
-			<Otsikko kurssiNimi={kurssi} />
-			<Sisalto osa1Nimi={osa1} osa2Nimi={osa2} osa3Nimi={osa3}
-			 osa1teht={tehtavia1} osa2teht={tehtavia2} osa3teht={tehtavia3} />
-			<Yhteensa tehtavaSumma={tehtavia1+tehtavia2+tehtavia3}/>
-		</div>
-		)
-}
-
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
 	)
+}
+ReactDOM.render(<App />, document.getElementById('root'))
